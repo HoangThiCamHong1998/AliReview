788ced9d9bf20853a17790dbe33fc60564174e94
FireGroup <FireGroup@LAPTOP-O2VE9B77>
2023-07-27 08:40:41 +0700

    add test_send_mail test suite

---
+++ Test Suites/Shopify/test_send_mail.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>test_send_mail</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>e8cc5027-2818-4743-8136-9a2c6eb12e99</testSuiteGuid>
+   <testCaseLink>
+      <guid>9d5cf579-94cc-430c-93d0-0e0042b9d9b8</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Shopify/TC04_Open login shopify page</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
